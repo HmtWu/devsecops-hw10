4a6b926166631612ed32baf22059c399e9a84811
hmt <hamiltonwu717@gmail.com>
2025-11-17 15:27:41 +0800

    Initial commit for DevSecOps demo

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-def add(a, b):
-    return a + b
+import os # 沒用到，Lint 會抓
+import subprocess # 用來示範之後的 SAST 安全問題
 
+def run_command(cmd):
+    # 這是一個危險的寫法，容易被 Command Injection
+    subprocess.call(cmd, shell=True)
 
-print(add(1, 2))
+def say_hello(): 
+    print("Hello") # 縮排不一致或格式問題
+    
+password = 'admin:123456' # 硬編碼密碼，SAST 會抓
